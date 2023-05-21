d96516efe37cbf566231b35756741344decdb45c
RajVekariya0 <115201171+RajVekariya0@users.noreply.github.com>
2023-05-21 17:52:34 -0400

    Content-list component, defined an erray of 6

--- Raj_Vekariya_MyFavouriteCricketer/src/app/app.module.ts
+++ Raj_Vekariya_MyFavouriteCricketer/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContentCardComponent } from './content-card/content-card.component';
+import { ContentListComponent } from './content-list/content-list.component'; // Add this line
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContentCardComponent
+    ContentListComponent // Add this line
   ],
   imports: [
     BrowserModule
